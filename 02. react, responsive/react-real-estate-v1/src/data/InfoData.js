9088aa65930e4f5de3bfa58efc56bed1352a107f
Ivan <i.jankovic@rocketmail.com>
2021-10-06 17:27:05 +0200

    prvi

---
+++ 02. react, responsive/react-real-estate-v1/src/data/InfoData.js
@@ -0,0 +1,22 @@
+import Image1 from '../images/interior1.jpg';
+import Image2 from '../images/interior2.jpg';
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragraphOne: 'Consequat est sit ut Lorem qui. Eiusmod est proident irure exercitation elit Lorem pariatur occaecat amet mollit exercitation anim. Eiusmod amet excepteur ex cupidatat quis enim adipisicing do do ut cupidatat. Do Lorem consectetur cupidatat excepteur et commodo nostrud eu. Irure veniam eu cupidatat duis minim deserunt anim culpa mollit occaecat commodo ex sit irure. Adipisicing sit cillum commodo deserunt reprehenderit duis voluptate elit nostrud eu ad ullamco nisi. Adipisicing proident nisi id velit ea eiusmod adipisicing ad mollit amet commodo eu.',
+    paragraphTwo: 'Reprehenderit nisi sunt pariatur culpa. Quis deserunt reprehenderit irure fugiat amet anim deserunt. Enim aliquip nulla aliquip cupidatat. Magna dolore deserunt aliquip voluptate ipsum. Nisi commodo ut minim et.',
+    buttonLabel: 'View homes',
+    image: Image1,
+    reverse: false,
+    delay: 100
+};
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragraphOne: 'Consequat est sit ut Lorem qui. Eiusmod est proident irure exercitation elit Lorem pariatur occaecat amet mollit exercitation anim. Eiusmod amet excepteur ex cupidatat quis enim adipisicing do do ut cupidatat. Do Lorem consectetur cupidatat excepteur et commodo nostrud eu.',
+    paragraphTwo: 'Reprehenderit nisi sunt pariatur culpa. Quis deserunt reprehenderit irure fugiat amet anim deserunt. Enim aliquip nulla aliquip cupidatat.',
+    buttonLabel: 'View homes',
+    image: Image2,
+    reverse: true,
+    delay: 300
+};
